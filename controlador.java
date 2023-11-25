026ed8a879ed27eefce02137c0b6046c4090cf8e
FilipeBem <discordso123@gmail.com>
2023-11-25 20:00:02 -0300

    initial commit

---
+++ controlador.java
@@ -0,0 +1,12 @@
+public interface controlador {
+    public abstract void ligar();
+    public abstract void desligar();
+    public abstract void abrirMenu();
+    public abstract void fecharMenu();
+    public abstract void maisVolume();
+    public abstract void menosVelume();
+    public abstract void ligarMudo();
+    public abstract void desligarMudo();
+    public abstract void play();
+    public abstract void palse();
+} 
---
+++ controle_main.java
@@ -0,0 +1,12 @@
+public class controle_main{
+    public static void main(String[] args) {
+        controleRemoto c = new controleRemoto();
+
+        c.ligar();
+        c.maisVolume();
+        c.play();
+        c.abrirMenu();
+        c.fecharMenu();
+        
+    }
+}
